5a07fd81f77dea8c046960642b013bc88b548839
sirbearnoir <SIRBEARNOIR@GMAIL.COM>
2023-02-02 16:33:22 -0500

    chat baby

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 console.log("Your index.js file is loaded correctly!");
 
-$("#myWork").on("click", function(){
+
+$("#myWork").on("click", function( ){
     console.log("you clicked on #myWork")
 });
